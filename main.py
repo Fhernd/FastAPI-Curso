7304ae3fe06c63172e8918a5a1adc8e8ade2b028
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-09-25 12:11:31 -0500

    Creando nuevas rutas con parámetros
    Creando nuevas rutas con parámetros de ruta y de consulta

--- main.py
+++ main.py
@@ -6,8 +6,19 @@ app = FastAPI()
 
 @app.get('/')
 def read_root():
-    return {'Hello': 'World!'}
+    return {'Hello': 'World!!!'}
+
 
 @app.get('/hola')
 def hola_mundo():
     return {'Hola': 'Mundo'}
+
+
+@app.get('/items/{item_id}')
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {'item_id': item_id, 'q': q}
+
+
+@app.get('/calculadora')
+def calcular(operando_1: float, operando_2: float):
+    return {'suma': operando_1 + operando_2}

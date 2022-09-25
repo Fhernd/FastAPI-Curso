e50c53164ee942aa8036f8ddbeca074a567c9df1
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-09-25 12:51:48 -0500

    Creada la ruta PUT para actualizar un ítem
    Creada la ruta PUT para actualizar un ítem

--- main.py
+++ main.py
@@ -30,3 +30,8 @@ def read_item(item_id: int, q: Union[str, None] = None):
 @app.get('/calculadora')
 def calcular(operando_1: float, operando_2: float):
     return {'suma': operando_1 + operando_2}
+
+
+@app.put('/items/{item_id}')
+def update_item(item_id: int, item: Item):
+    return {'item_name': item.name, 'item_id': item_id, 'item_price': item.price}

13d38f86b8d856fb47af7e38db314937d3f87077
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-16 19:28:00 -0500

    Ajustando la función

--- S04_types/s04_03_deteccion_errores.py
+++ S04_types/s04_03_deteccion_errores.py
@@ -1,3 +1,6 @@
 def get_name_with_age(name: str, age: int):
-    name_with_age = name + ' is this old: ' + age
+    name_with_age = name + ' is this old: ' + str(age)
     return name_with_age
+
+
+print(get_name_with_age('John', 29))

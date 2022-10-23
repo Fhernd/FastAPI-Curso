410fff3a98287f76692e9dd2fd928d3e01d239af
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-23 18:23:08 -0500

    Ruta para buscar un producto a partir de su ID

--- practicas_activas/api_productos.py
+++ practicas_activas/api_productos.py
@@ -32,3 +32,13 @@ def crear_producto(producto: Producto):
     producto.id = str(uuid())
     productos.append(producto)
     return {'mensaje': 'Producto creado satisfactoriamente.'}
+
+
+@app.get('/producto/{producto_id}')
+def obtener_producto_por_id(producto_id: str):
+    for p in productos:
+        if p.id == producto_id:
+            return p
+    
+    return {'mensaje': f'El producto con el ID {producto_id} no fue encontrado.'}
+

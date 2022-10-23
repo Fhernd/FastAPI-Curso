f13b623be5376fdbd52cc421bd75b1bb62d0e8eb
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-23 18:01:36 -0500

    Clase para representar un producto

--- practicas_activas/api_productos.py
+++ practicas_activas/api_productos.py
@@ -1,7 +1,19 @@
+from typing import Optional
 from fastapi import FastAPI
+
+from pydantic import BaseModel
+
+class Producto(BaseModel):
+    id: Optional[str]
+    nombre: str
+    precio_compra: float
+    precio_venta: float
+    proveedor: str
 
 
 app = FastAPI()
+
+productos = []
 
 
 @app.get('/')

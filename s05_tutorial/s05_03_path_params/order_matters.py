bb73ddc5d1bee9df86a5b5d4b04522afc025ca19
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-11-01 19:11:57 -0500

    El orden importa a la hora de especificar rutas

---
+++ s05_tutorial/s05_03_path_params/order_matters.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/users/me')
+async def read_user_me():
+    return {'user_id': 'The current user'}
+
+
+@app.get('/users/{user_id}')
+async def read_user(user_id: str):
+    return {'user_id': user_id}

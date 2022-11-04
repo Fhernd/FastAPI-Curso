58b9c36da0b2e325e45e1cf29e3d6c80fea926af
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-11-04 18:06:44 -0500

    Prueba para parámetros de consulta

---
+++ s05_tutorial/s05_04_query_params/intro_query_params.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+fake_items_db = [
+    {'item_name': 'foo'},
+    {'item_name': 'Bar'},
+    {'item_name': 'Baz'},
+]
+
+
+@app.get('/items/')
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip: skip + limit]

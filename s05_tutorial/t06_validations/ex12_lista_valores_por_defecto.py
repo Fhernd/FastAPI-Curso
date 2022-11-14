82041ae74bdd637694ab40bf62f09d5b7df21b47
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-11-14 07:14:32 -0500

    Lista vacía como valor por defecto

---
+++ s05_tutorial/t06_validations/ex12_lista_valores_por_defecto.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI, Query
+
+app = FastAPI()
+
+
+@app.get('/items/')
+async def read_items(q: list = Query(default=[])):
+    query_items = {'q': q}
+
+    return query_items

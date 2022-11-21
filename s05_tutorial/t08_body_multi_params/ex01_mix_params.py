5cd5af8538765e196c245739e1a151775adc573e
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-11-20 20:50:25 -0500

    Parámetros de tipo path, query y body request

---
+++ s05_tutorial/t08_body_multi_params/ex01_mix_params.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI, Path
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+
+@app.put('/items/{item_id}')
+async def update_item(*, item_id: int = Path(title='The ID of the item to get', ge=0, le=1000), q: str | None = None, item: Item | None = None):
+    result = {'item_id': item_id}
+
+    if q:
+        result.update({'q': q})
+    
+    if item:
+        result.update({'item': item})
+    
+    return result

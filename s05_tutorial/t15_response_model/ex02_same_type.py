7f11cf7fe3e5c68bda57b67c05c31131f5fe5633
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-12-15 18:46:56 -0500

    Mismo tipo de dato para entrada y salida

---
+++ s05_tutorial/t15_response_model/ex02_same_type.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from pydantic import BaseModel, EmailStr
+
+app = FastAPI()
+
+
+class UserIn(BaseModel):
+    username: str
+    password: str
+    email: EmailStr
+    full_name: str | None = None
+
+
+@app.post("/users/", response_model=UserIn)
+async def create_user(user: UserIn):
+    return user

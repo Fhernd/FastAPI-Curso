ef17407736a919649a67b7e7c154a57323dbbe85
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-12-22 19:17:43 -0500

    Incluir un encabezado personalizado en la respuesta

---
+++ s05_tutorial/t21_handling_erros/ex03_headers.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI, HTTPException, status
+
+app = FastAPI()
+
+items = {"foo": "The Foo Wrestlers"}
+
+
+@app.get("/items-header/{item_id}")
+async def read_item(item_id: str):
+    if item_id not in items:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail={"error": "Item not found", "item_id": item_id, 'code': status.HTTP_404_NOT_FOUND},
+            headers={"X-Error": "There goes my error"},
+        )
+    
+    return {"item": items[item_id]}

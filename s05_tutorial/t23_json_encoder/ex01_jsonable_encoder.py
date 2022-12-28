07a269e4d6bea952fc603068262071b1a69088b7
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-12-27 21:04:06 -0500

    Codificar en formato JSON

---
+++ s05_tutorial/t23_json_encoder/ex01_jsonable_encoder.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+
+from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel
+
+fake_db = {}
+
+
+class Item(BaseModel):
+    title: str
+    timestamp: datetime
+    description: str = None
+
+
+app = FastAPI()
+
+
+@app.put("/items/{id}")
+async def update_item(id: str, item: Item):
+    json_compatible_item_data = jsonable_encoder(item)
+    fake_db[id] = json_compatible_item_data
+    return fake_db[id]
+
+
+@app.get("/items/")
+async def read_items():
+    return fake_db

13b77971bdfe95e6c9d3f2fba67e2717077e43d3
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-12-27 20:43:43 -0500

    Agregar una descripción a la respuesta

---
+++ s05_tutorial/t22_path_op_config/ex06_response_description.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+    tags: set[str] = set()
+
+
+@app.post(
+    "/items/",
+    response_model=Item,
+    tags=["items"],
+    summary="Create an item and store it in the database",
+    response_description="The created item")
+async def create_item(item: Item):
+    """
+    Create an item with all the information, name, description, price, tax and a set of unique tags
+
+    - **name**: each item must have a name
+    - **description**: a description of the item
+    - **price**: the price must be a positive float
+    - **tax**: the tax must be a positive float
+    - **tags**: a set of unique tags for this item
+    """
+    return item

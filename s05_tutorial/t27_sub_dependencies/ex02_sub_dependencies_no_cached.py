9a0bcd2537f8b4af206ff138c573a578f32fca6d
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2023-01-02 21:22:28 -0500

    Dependencias sin usar caché

---
+++ s05_tutorial/t27_sub_dependencies/ex02_sub_dependencies_no_cached.py
@@ -0,0 +1,20 @@
+from fastapi import Cookie, Depends, FastAPI
+
+app = FastAPI()
+
+
+def query_extractor(q: str | None = None):
+    return q
+
+
+def query_or_cookie_extractor(
+    q: str = Depends(query_extractor), last_query: str | None = Cookie(None)
+):
+    if not q:
+        return last_query
+    return q
+
+
+@app.get("/items/")
+async def read_items(query_or_default: str = Depends(query_or_cookie_extractor, use_cache=False)):
+    return {'q_or_cookie': query_or_default}

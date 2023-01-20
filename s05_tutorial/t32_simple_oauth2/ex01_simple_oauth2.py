4b3d41aef832fcb2ee8b3d380b8081bb5f3fd13d
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2023-01-19 19:05:11 -0500

    Implementando OAuth2 simple

---
+++ s05_tutorial/t32_simple_oauth2/ex01_simple_oauth2.py
@@ -0,0 +1,21 @@
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from pydantic import BaseModel
+
+
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "fakehashedsecret",
+        "disabled": False,
+    },
+    "alice": {
+        "username": "alice",
+        "full_name": "Alice Wonderson",
+        "email": "alice@example.com",
+        "hashed_password": "fakehashedsecret2",
+        "disabled": True,
+    },
+}

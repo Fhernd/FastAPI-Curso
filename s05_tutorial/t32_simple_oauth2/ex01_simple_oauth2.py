b8144605894ed84375f4fd3e4a70f97485735697
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2023-01-22 20:35:58 -0500

    Obtener el usuario de la base de datos

--- s05_tutorial/t32_simple_oauth2/ex01_simple_oauth2.py
+++ s05_tutorial/t32_simple_oauth2/ex01_simple_oauth2.py
@@ -19,3 +19,29 @@ fake_users_db = {
         "disabled": True,
     },
 }
+
+app = FastAPI()
+
+
+def fake_hash_password(password: str):
+    return "fakehashed" + password
+
+
+ouath2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+class User(BaseModel):
+    username: str
+    email: str | None = None
+    full_name: str | None = None
+    disabled: bool | None = None
+
+
+class UserInDB(User):
+    hashed_password: str
+
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)

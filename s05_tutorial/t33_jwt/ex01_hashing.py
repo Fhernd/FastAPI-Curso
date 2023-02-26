89ec1e9178bd772460cd7106ab6f2103b4a9ff0b
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2023-02-26 18:53:43 -0500

    Obtener el usuario de la información del token

--- s05_tutorial/t33_jwt/ex01_hashing.py
+++ s05_tutorial/t33_jwt/ex01_hashing.py
@@ -105,15 +105,30 @@ def create_access_token(data: dict, expires_delta: timedelta = None):
 
 async def get_current_user(token: str = Depends(ouath2_scheme)):
     """
-    Simula una función que obtiene un usuario actual a partir de un token.    
+    Obtiene el usuario actual a partir de un token.
+
+    :param token: Token de acceso.
     """
-    user = fake_decode_token(token)
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    
+    user = get_user(fake_users_db, username=token_data.username)
+
     if user is None:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid authentication credentials",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
+        raise credentials_exception
+    
     return user
 
 

a13e7a7ae0c2a5048b8775b95aca253d8054b280
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2023-03-12 19:19:51 -0500

    Operación de ruta para cargar datos (simulación)

--- s05_tutorial/t34_middleware/ex01_middleware.py
+++ s05_tutorial/t34_middleware/ex01_middleware.py
@@ -1,3 +1,4 @@
+from random import randint
 import time
 
 from fastapi import FastAPI, Request
@@ -7,8 +8,21 @@ app = FastAPI()
 
 @app.middleware("http")
 async def add_process_time_header(request: Request, call_next):
+    """
+    Middleware que añade el tiempo de procesamiento de la petición.
+    """
     start_time = time.time()
     response = await call_next(request)
     process_time = time.time() - start_time
     response.headers["X-Process-Time"] = str(process_time)
     return response
+
+
+@app.get("/cargar-datos")
+async def cargar_datos():
+    """
+    Simula la carga de datos.
+    """
+    time.sleep(randint(3, 7))
+    datos = {"datos": "cargados"}
+    return datos

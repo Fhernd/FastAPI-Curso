0f744a8185e85453f3568cf483a6ecf2d162712a
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2023-03-20 16:36:03 -0500

    Permitir un nuevo origin

--- s05_tutorial/t35_cors/ex01_cors_middleware.py
+++ s05_tutorial/t35_cors/ex01_cors_middleware.py
@@ -7,7 +7,9 @@ origins = [
     "http://localhost",
     "http://localhost:8080",
     "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com"
+    "https://localhost.tiangolo.com",
+    "http://localhost:5501",
+    "http://127.0.0.1:5501"
 ]
 
 app.add_middleware(

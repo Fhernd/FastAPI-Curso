fe3d425b3d460e8815427e6208b8258f4b1f7ac6
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2023-03-20 16:32:16 -0500

    API con CORS

---
+++ s05_tutorial/t35_cors/ex01_cors_middleware.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+async def main():
+    return {"message": "Hello World"}

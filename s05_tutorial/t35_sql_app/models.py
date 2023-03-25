7708a4da5beaea8d7b0d8ceb94f76b8cc4be5a75
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2023-03-24 19:37:40 -0500

    Definido el modelo ORM para la tabla items

--- s05_tutorial/t35_sql_app/models.py
+++ s05_tutorial/t35_sql_app/models.py
@@ -13,3 +13,14 @@ class User(Base):
     is_active = Column(Boolean, default=True)
 
     items = relationship("Item", back_populates="owner")
+
+
+class Item(Base):
+    __tablename__ = "items"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String, index=True)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+
+    owner = relationship("User", back_populates="items")

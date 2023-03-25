0679b43c0267dd7541365158f6f2fdf397dd1a69
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2023-03-24 19:27:45 -0500

    Creando el modelo de ORM para la tabla users

---
+++ s05_tutorial/t35_sql_app/models.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+
+from .database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+
+    items = relationship("Item", back_populates="owner")

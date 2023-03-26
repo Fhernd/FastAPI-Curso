95cb6b0af394b2b03eccec31cabf142eba834877
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2023-03-26 18:32:49 -0500

    Asignación de documentación para los esquemas de Pydantic

--- s05_tutorial/t35_sql_app/schemas.py
+++ s05_tutorial/t35_sql_app/schemas.py
@@ -2,15 +2,24 @@ from pydantic import BaseModel
 
 
 class ItemBase(BaseModel):
+    """
+    Base class for Item.
+    """
     title: str
     description: str | None = None
 
 
 class ItemCreate(ItemBase):
+    """
+    Class for creating Item.
+    """
     pass
 
 
 class Item(ItemBase):
+    """
+    Class for Item.
+    """
     id: int
     owner_id: int
 
@@ -19,14 +28,23 @@ class Item(ItemBase):
 
 
 class UserBase(BaseModel):
+    """
+    Base class for User.
+    """
     email: str
 
 
 class UserCreate(UserBase):
+    """
+    Class for creating User.
+    """
     password: str
 
 
 class User(UserBase):
+    """
+    Class for User.
+    """
     id: int
     is_active: bool
     items: list[Item] = []

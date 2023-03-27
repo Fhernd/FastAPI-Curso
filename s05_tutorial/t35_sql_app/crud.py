dd254151a167f06716df63a83112c8d095a86d3f
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2023-03-26 19:14:26 -0500

    Función para obtener un usuario usando su email

--- s05_tutorial/t35_sql_app/crud.py
+++ s05_tutorial/t35_sql_app/crud.py
@@ -11,3 +11,13 @@ def get_user(db: Session, user_id: int):
     :param user_id: user ID
     """
     return db.query(models.User).filter(models.User.id == user_id).first()
+
+
+def get_user_by_email(db: Session, email: str):
+    """
+    Get user by email.
+
+    :param db: database session
+    :param email: user email
+    """
+    return db.query(models.User).filter(models.User.email == email).first()

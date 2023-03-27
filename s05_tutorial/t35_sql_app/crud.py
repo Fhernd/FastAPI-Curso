167d0589765812ac18d44de3edbcfa30c47bf122
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2023-03-26 19:21:07 -0500

    Función para traer N registros de la tabla users

--- s05_tutorial/t35_sql_app/crud.py
+++ s05_tutorial/t35_sql_app/crud.py
@@ -21,3 +21,14 @@ def get_user_by_email(db: Session, email: str):
     :param email: user email
     """
     return db.query(models.User).filter(models.User.email == email).first()
+
+
+def get_users(db: Session, skip: int = 0, limit: int = 100):
+    """
+    Get users.
+
+    :param db: database session
+    :param skip: skip first N users
+    :param limit: limit users to N
+    """
+    return db.query(models.User).offset(skip).limit(limit).all()

933dc3e249a2177fee1924c52da7560cb54f7447
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2023-03-29 20:14:19 -0500

    Función de operación de ruta para crear usuarios

--- s05_tutorial/t35_sql_app/main.py
+++ s05_tutorial/t35_sql_app/main.py
@@ -21,3 +21,21 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+@app.post("/users/", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    """
+    Create user.
+
+    :param user: User to create
+    :param db: Database session.
+
+    :return: Created user.
+    """
+    db_user = crud.get_user_by_email(db, email=user.email)
+    
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    
+    return crud.create_user(db=db, user=user)

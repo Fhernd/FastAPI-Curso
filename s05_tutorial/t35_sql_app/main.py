babb64257f7d4c172422d0c704ec45bbc0e05a06
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2023-04-06 18:32:53 -0500

    Operación de ruta tipo GET para obtener un usuario usando su ID

--- s05_tutorial/t35_sql_app/main.py
+++ s05_tutorial/t35_sql_app/main.py
@@ -54,3 +54,21 @@ def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     """
     users = crud.get_users(db, skip=skip, limit=limit)
     return users
+
+
+@app.get("/users/{user_id}", response_model=schemas.User)
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    """
+    Get user by id.
+
+    :param user_id: user ID
+    :param db: database session
+
+    :return: User with given ID or None if not found.
+    """
+    db_user = crud.get_user(db, user_id=user_id)
+    
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    
+    return db_user

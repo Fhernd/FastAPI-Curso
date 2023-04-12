af7311c43be8ff9019b7953d62d2030af400cec4
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2023-04-12 18:24:06 -0500

    Documentación básica para los elementos del módulo database

--- s05_tutorial/t35_sql_app/database.py
+++ s05_tutorial/t35_sql_app/database.py
@@ -2,12 +2,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+# URL de base de datos SQLite:
 SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 
+# Crea el motor de base de datos:
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
 
+# Crea una sesión de base de datos:
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+# Crea una clase base para declarar clases de modelo SQLAlchemy:
 Base = declarative_base()

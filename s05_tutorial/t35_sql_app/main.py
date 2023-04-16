80502fb162aa621135b6aa1b9de7dddd4915604b
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2023-04-16 12:24:20 -0500

    Middleware para la conexión a la base de datos

--- s05_tutorial/t35_sql_app/main.py
+++ s05_tutorial/t35_sql_app/main.py
@@ -1,4 +1,4 @@
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException, Request, Response
 from sqlalchemy.orm import Session
 
 import crud
@@ -9,6 +9,19 @@ from database import SessionLocal, engine
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+
+@app.middleware('http')
+async def db_session_middleware(request: Request, call_next):
+    response = Response("Internal server error", status_code=500)
+
+    try:
+        request.state.db = SessionLocal()
+        response = await call_next(request)
+    finally:
+        request.state.db.close()
+    
+    return response
 
 
 def get_db():

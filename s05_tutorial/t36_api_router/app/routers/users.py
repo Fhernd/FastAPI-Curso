2126ce96a5eac356af93474f0dcb254c917e5ba9
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2023-04-19 17:31:47 -0500

    Obtiene el usuario actual

--- s05_tutorial/t36_api_router/app/routers/users.py
+++ s05_tutorial/t36_api_router/app/routers/users.py
@@ -12,3 +12,12 @@ async def read_users():
     """
     return [{"username": "Rick"}, {"username": "Morty"}]
 
+
+@router.get("/users/me", tags=["users"])
+async def read_user_me():
+    """
+    Get current user.
+
+    -returns: current user
+    """
+    return {"username": "fakecurrentuser"}

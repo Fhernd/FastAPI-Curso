e595316571f22ea9c244f49975419b919a170145
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2023-04-23 19:13:16 -0500

    Obtiene el token de la consulta

--- s05_tutorial/t36_api_router/app/dependencies.py
+++ s05_tutorial/t36_api_router/app/dependencies.py
@@ -12,3 +12,13 @@ async def get_token_header(x_token: Annotated[str, Header()]):
     if x_token != 'fake-super-secret-token':
         raise HTTPException(status_code=400, detail='X-Token header invalid')
 
+
+async def get_query_token(token: str):
+    """
+    Get query token.
+
+    :param: token: Query token
+    -returns: query token
+    """
+    if token != 'jessica':
+        raise HTTPException(status_code=400, detail='Query token invalid')

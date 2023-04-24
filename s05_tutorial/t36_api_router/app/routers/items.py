5d05be5f4e260fdcffa502df8a435272f2bc8535
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2023-04-23 19:19:05 -0500

    Operación de ruta para obtener todos los ítems

--- s05_tutorial/t36_api_router/app/routers/items.py
+++ s05_tutorial/t36_api_router/app/routers/items.py
@@ -9,3 +9,18 @@ router = APIRouter(
     dependencies=[Depends(get_token_header)],
     responses={404: {'description': 'Not found'}},
 )
+
+
+fake_items_db = {"plumbus": {"name": "Plumbus"}, "gun": {"name": "Portal Gun"}}
+
+
+@router.get('/')
+async def read_items():
+    """
+    Get all items.
+
+    -returns: a list of items
+    """
+    return fake_items_db
+
+

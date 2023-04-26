6548ffe7b61bc6dc2f97a6f0051d27c13c80ecf8
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2023-04-26 18:18:02 -0500

    Importación de routers en el archivo principal de la API

--- s05_tutorial/t36_api_router/app/main.py
+++ s05_tutorial/t36_api_router/app/main.py
@@ -2,9 +2,27 @@ from fastapi import Depends, FastAPI
 
 from .dependencies import get_query_token, get_token_header
 from .routers import items, users
+from .internal import admin
 
 app = FastAPI(dependencies=[Depends(get_query_token)])
 
 
 app.include_router(users.router)
 app.include_router(items.router)
+app.include_router(
+    admin.router, 
+    prefix='/admin', 
+    tags=['admin'],
+    dependencies=[Depends(get_token_header)],
+    responses={418: {'description': 'I am a teapot'}},
+)
+
+
+@app.get('/')
+async def root():
+    """
+    Root path.
+    
+    -returns: a message
+    """
+    return {'message': 'Hello Bigger Applications!'}

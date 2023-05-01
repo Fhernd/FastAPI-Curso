6655a9f58f8b11708e249607a56b53c1d3dc7682
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2023-04-30 20:21:14 -0500

    Operaciones de ruta para el módulo admin

--- s05_tutorial/t36_api_router/app/internal/admin.py
+++ s05_tutorial/t36_api_router/app/internal/admin.py
@@ -1,3 +1,23 @@
 from fastapi import APIRouter
 
 router = APIRouter()
+
+
+@router.get("/admin", tags=["users"])
+async def read_admin():
+    """
+    Get admin.
+    
+    -returns: admin
+    """
+    return {"username": "admin"}
+
+
+@router.get("/admin/me", tags=["users"])
+async def read_admin_me():
+    """
+    Get current admin.
+    
+    -returns: current admin
+    """
+    return {"username": "fakecurrentadmin"}

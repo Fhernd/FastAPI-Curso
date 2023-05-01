1984d0c5983b1aaf7c83dc42fafc2d287fc0ff38
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2023-05-01 11:09:48 -0500

    Operación de ruta para simular el envío de un correo electrónico

--- s05_tutorial/t37_background_tasks/ex01_background_tasks.py
+++ s05_tutorial/t37_background_tasks/ex01_background_tasks.py
@@ -3,7 +3,7 @@ from fastapi import BackgroundTasks, FastAPI
 app = FastAPI()
 
 
-def write_notiification(email: str, message=''):
+def write_notification(email: str, message=''):
     """
     Write notification to a file
     
@@ -13,3 +13,17 @@ def write_notiification(email: str, message=''):
     with open('log.txt', mode='w') as email_file:
         content = f'notification for {email}: {message}\n'
         email_file.write(content)
+
+@app.post('/send-notification/{email}')
+async def send_notification(email: str, background_tasks: BackgroundTasks):
+    """
+    Send notification to a user
+    
+    :param email: email address
+    :param background_tasks: background tasks
+    
+    :return: message
+    """
+    background_tasks.add_task(write_notification, email, message='some notification')
+    
+    return {'message': 'Notification sent in the background'}

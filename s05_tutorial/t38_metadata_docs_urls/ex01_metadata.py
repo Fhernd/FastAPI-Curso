0eccebd1ef0c67db6977fffa1948716f9c9cfa14
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2023-05-03 18:23:57 -0500

    Operación de ruta para mostrar un listado de ítems ficticios

--- s05_tutorial/t38_metadata_docs_urls/ex01_metadata.py
+++ s05_tutorial/t38_metadata_docs_urls/ex01_metadata.py
@@ -32,3 +32,11 @@ app = FastAPI(
 )
 
 
+@app.get('/items/')
+async def read_items():
+    """
+    Read items with all the information.
+    
+    :returns: List of items.
+    """
+    return [{'name': 'Foo'}]

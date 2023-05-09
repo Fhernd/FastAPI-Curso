d4d31fba31617c32e535276ae039cdea2247bf0e
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2023-05-09 18:36:40 -0500

    No se ha especificado el token de seguridad correcto

--- s05_tutorial/t40_testing/application/test_main.py
+++ s05_tutorial/t40_testing/application/test_main.py
@@ -6,6 +6,10 @@ client = TestClient(app)
 
 
 def test_read_item():
+    """
+    Check that the endpoint returns the correct item.
+    """
+    
     response = client.get("/items/foo", headers={"X-Token": "llosavargasmario"})
     
     assert response.status_code == 200
@@ -14,4 +18,18 @@ def test_read_item():
         'id': 'foo',
         'title': 'Foo',
         'description': 'There goes my hero'
+    }
+
+
+def test_read_item_bad_token():
+    """
+    Check that the endpoint returns an error if the token is invalid.
+    """
+    
+    response = client.get("/items/foo", headers={"X-Token": "mendozamario"})
+    
+    assert response.status_code == 400
+    
+    assert response.json() == {
+        'detail': 'X-Token header invalid'
     }

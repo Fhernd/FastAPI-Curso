dcf94ab7329910ad101d0993ecdba37b7853d7f4
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2023-06-20 21:39:43 -0500

    Depuración de un endpoint

---
+++ s05_tutorial/t41_debugging/ex01_uvicorn_debugging.py
@@ -0,0 +1,19 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def root():
+    """
+    Check that the endpoint returns the correct item.
+    """
+    a = 'a'
+    b = 'b' + a
+    
+    return {'hello world': b}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)

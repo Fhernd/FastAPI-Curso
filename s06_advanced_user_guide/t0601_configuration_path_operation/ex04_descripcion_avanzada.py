1b35c6457e5e556974fd356792dfe44ee4c26be5
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2023-06-27 20:45:38 -0500

    Creación del modelo que representa la entidad ítem

---
+++ s06_advanced_user_guide/t0601_configuration_path_operation/ex04_descripcion_avanzada.py
@@ -0,0 +1,14 @@
+from typing import Set, Union
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+    tags: Set[str] = set()

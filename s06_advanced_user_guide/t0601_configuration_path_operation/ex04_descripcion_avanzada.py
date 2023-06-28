c0612e88f2f06fe8fda9c1492a60c247e85c1b84
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2023-06-27 20:46:52 -0500

    Descripción avanzada para una operación de ruta

--- s06_advanced_user_guide/t0601_configuration_path_operation/ex04_descripcion_avanzada.py
+++ s06_advanced_user_guide/t0601_configuration_path_operation/ex04_descripcion_avanzada.py
@@ -12,3 +12,19 @@ class Item(BaseModel):
     price: float
     tax: Union[float, None] = None
     tags: Set[str] = set()
+
+
+@app.post("/items/", response_model=Item, summary="Create an item")
+async def create_item(item: Item):
+    """
+    Create an item with all the information:
+
+    - **name**: each item must have a name
+    - **description**: a long description
+    - **price**: required
+    - **tax**: if the item doesn't have tax, you can omit this
+    - **tags**: a set of unique tag strings for this item
+    \f
+    :param item: User input.
+    """
+    return item

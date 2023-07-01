9d3dc9eda019b86094d1f250d0b9e26e1199c42e
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2023-06-30 20:13:54 -0500

    Asignación de descripción a una API

--- s06_advanced_user_guide/t0601_configuration_path_operation/ex05_openapi_extra.py
+++ s06_advanced_user_guide/t0601_configuration_path_operation/ex05_openapi_extra.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(
+    title='My Super Project',
+    openapi_extra={
+        "info": {
+            "contact": {
+                "name": "Soporte",
+                "url": "https://example.com/contacto",
+                "email": "soporte@example.com",
+            },
+            "termsOfService": "https://example.com/terminos",
+        },
+    },
+)
 
 
 @app.get("/items/", openapi_extra={"x-aperture-labs-portal": "blue"})

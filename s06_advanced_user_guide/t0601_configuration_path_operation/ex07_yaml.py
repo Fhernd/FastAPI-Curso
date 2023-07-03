3c79dcdf83de128e8f0a827573614fd7205f73cd
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2023-07-02 19:14:13 -0500

    Uso de YAML para especificar el esquema del body en una solicitud

---
+++ s06_advanced_user_guide/t0601_configuration_path_operation/ex07_yaml.py
@@ -0,0 +1,32 @@
+import yaml
+from fastapi import FastAPI, HTTPException, Request
+from pydantic import BaseModel, ValidationError
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    tags: List[str]
+
+
+@app.post("/items/", openapi_extra={
+        "requestBody": {
+            "content": {"application/x-yaml": {"schema": Item.schema()}},
+            "required": True,
+        },
+    }
+)
+async def create_item(request: Request):
+    raw_body = await request.body()
+    try:
+        data = yaml.safe_load(raw_body)
+    except yaml.YAMLError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+    try:
+        item = Item.parse_obj(data)
+    except ValidationError as e:
+        raise HTTPException(status_code=422, detail=e.errors())
+    
+    return item

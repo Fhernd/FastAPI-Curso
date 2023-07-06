cdb603e77c42629ad912661185d1577e5b0c5676
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2023-07-05 21:05:42 -0500

    Inclusión de más contenido HTML para la respuesta

--- s06_advanced_user_guide/t0604_custom_response/ex02_html_response.py
+++ s06_advanced_user_guide/t0604_custom_response/ex02_html_response.py
@@ -13,6 +13,19 @@ async def read_items():
         </head>
         <body>
             <h1>Look ma! HTML!</h1>
+            <img src="https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif" />
+            
+            <table border="1">
+                <tbody>
+                    <tr>
+                        <td>Foo</td>
+                        <td>Bar</td>
+                    </tr>
+                    <tr>
+                        <td>Foo</td>
+                        <td>Bar</td>
+                    </tr>
+            </table>
         </body>
     </html>
     """

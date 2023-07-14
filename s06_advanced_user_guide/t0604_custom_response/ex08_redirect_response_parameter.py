18859e6bb5ac2c6d3ecdebe13fde163ae327b9e0
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2023-07-13 20:47:54 -0500

    Documentación para la función de redireccionamiento

--- s06_advanced_user_guide/t0604_custom_response/ex08_redirect_response_parameter.py
+++ s06_advanced_user_guide/t0604_custom_response/ex08_redirect_response_parameter.py
@@ -6,4 +6,7 @@ app = FastAPI()
 
 @app.get('/typer/', response_class=RedirectResponse)
 async def redirect_typer():
+    """
+    Redirect to a website.
+    """
     return 'https://ortizol.blogspot.com'

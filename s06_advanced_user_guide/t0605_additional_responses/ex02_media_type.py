5a1894d1622658a0cb3447aba0eef57a736bf3d8
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2023-07-19 19:57:41 -0500

    Imagen para mostrar en lugar de un contenido JSON

--- s06_advanced_user_guide/t0605_additional_responses/ex02_media_type.py
+++ s06_advanced_user_guide/t0605_additional_responses/ex02_media_type.py
@@ -16,7 +16,7 @@ app = FastAPI()
 @app.get('items/{item_id}', response_model=Item, responses={200: {"content": {'image/png': {}}, 'description': "Return the JSON item or an image."}})
 async def read_item(item_id: str, img: Union[bool, None] = None):
     if img:
-        FileResponse('image.png', media_type='image/png')
+        FileResponse('imagen.png', media_type='image/png')
     else:
         return {
             'id': 'foo',

52566b1e73eea00ec5b09fe1a2a5b708919b28f9
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2023-07-19 20:00:22 -0500

    Solución de problema relacionado con la sentencia return

--- s06_advanced_user_guide/t0605_additional_responses/ex02_media_type.py
+++ s06_advanced_user_guide/t0605_additional_responses/ex02_media_type.py
@@ -13,10 +13,10 @@ class Item(BaseModel):
 app = FastAPI()
 
 
-@app.get('items/{item_id}', response_model=Item, responses={200: {"content": {'image/png': {}}, 'description': "Return the JSON item or an image."}})
+@app.get('/items/{item_id}', response_model=Item, responses={200: {"content": {'image/png': {}}, 'description': "Return the JSON item or an image."}})
 async def read_item(item_id: str, img: Union[bool, None] = None):
     if img:
-        FileResponse('imagen.png', media_type='image/png')
+        return FileResponse('imagen.png', media_type='image/png')
     else:
         return {
             'id': 'foo',
